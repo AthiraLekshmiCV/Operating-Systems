4e97aec4fd49d4c06813cd4af8fcd2b329200e3e
Lekshmi <andnlnbn18@gmail.com>
2014-07-31 18:16:34 +0530

    Added new

---
+++ Shell-Scripting/name.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+<<COMMENT
+Write a script that will take a person's name as a parameter to the program name. 
+The script should greet that person, as "Good Day name."
+COMMENT
+
+echo "Enter your name"
+read name
+echo "Good Day $name"
---
+++ Shell-Scripting/time.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# Write a script that would wait 5 seconds and then display the time
+
+    sleep 5
+    date +%r
+
+
+
