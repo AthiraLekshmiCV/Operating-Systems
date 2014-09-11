e54574d20228cfbd5f578e47fe7f7f47da384e12
Lekshmi <andnlnbn18@gmail.com>
2014-09-11 18:53:00 +0530

    Shell script to print prime numbers up to 300

---
+++ primeme.sh
@@ -0,0 +1,34 @@
+# !/bin/sh
+
+<<COMMENT
+Script that prints the prime numbers from 3 to 300
+COMMENT
+
+start=3
+stop=300
+
+echo "2"
+
+while [ $start -ne $stop ]
+do
+	temp=`expr $start / 2 `  
+	flag=0			
+	t=$((start + 0))    
+	i=2  
+	while [ $i -le $temp ]   
+	do
+		check=`expr $t % $i`
+		if [ $check -eq 0 -a $flag -eq 0 ]		
+		then
+
+			flag=1
+		fi		
+		i=$((i + 1))
+	done 
+	if [ $flag -eq 0 ]
+	then
+		echo "$start"
+	fi
+	start=`expr $start + 1 `
+done
+
